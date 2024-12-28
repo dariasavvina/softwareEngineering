2d3477103563d2c300813dc3664975a0226a78b5
Yaroslav <yaraslau.komarov@yandex.ru>
2024-12-28 16:48:37 +0500

    DZ-2-implement-yolo-controller

--- api.py
+++ api.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
+import requests
+from fastapi import FastAPI, UploadFile, File
 from pydantic import BaseModel
-from model import RecipesGeneratingParameters
+from model import RecipesGeneratingParameters, ImageObjectsResponse
 from recipe_generator import generation_recipes
+
 
 app = FastAPI()
 
+YOLOv10_URl = "https://8001/yolov10"
 
 class RecipesGeneratingParametersBody(BaseModel):
     count_recipes: int
@@ -17,3 +20,16 @@ def generate_recipes(body: RecipesGeneratingParametersBody):
     parameters = RecipesGeneratingParameters(body.count_recipes, body.theme, body.products)
     recipes = generation_recipes(parameters)
     return recipes
+
+
+@app.post("/image_objects")
+async def process_image(file: UploadFile = File(...)):
+    image_data = await file.read()
+
+    response = requests.post(YOLOv10_URl, files={"file": ("image.jpg", image_data, "image/jpeg")})
+    ImageObjectsResponse
+
+    if response.status_code == 200:
+        return ImageObjectsResponse(response = response.text)
+    else:
+        return {"error": "Failed to process image"}
